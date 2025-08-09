9e38ac660291b3956c77f083c2ed502aa7a50101
Professor 👨‍🏫📚 <oladoyeheritage445@gmail.com>
2025-08-09 22:33:33 +0100

    Update script.js

--- script.js
+++ script.js
@@ -516,7 +516,7 @@ const hour = new Date().getHours();
   chatHistory.push({ role: "assistant", content: stripHTML(welcome) });
 
   askForName();
-});
+
 
 // Expose functions globally
 window.sendMessage = sendMessage;
